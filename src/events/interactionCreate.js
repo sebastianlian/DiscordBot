999ca59a1e35f15f9be3fd63cb97ccf4c73ac6c1
Alexis Pardo <alexispardo27@gmail.com>
2023-09-28 16:43:07 -0400

    Creates event handlers for "ready" and "interactionCreate"

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "interactionCreate",
+    once: true,
+    async execute (interaction) {
+        //checks if a chat message is a valid command
+        if(!interaction.isCommand()){
+            return
+        }
+    
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if(!command){
+            return;
+        }
+    
+        try {
+            await command.execute(interaction);
+        } catch(err) {
+            if (err) {
+                console.error(err);
+    
+                await interaction.reply({
+                    content: "An error has occurred",
+                    ephemeral: true
+                });
+            }
+        }
+    }
+}
