6e68d24116910b546f674cfe4cc602be47e08e59
Chidiebube <ebubeokebalama@gmail.com>
2023-10-05 14:43:50 -0400

    Updated showInactivity
    showInactivity no longer takes input anymore.

--- src/commands/showInactivity.js
+++ src/commands/showInactivity.js
@@ -4,18 +4,24 @@ module.exports = {
 	data: new SlashCommandBuilder()
 	//setName cannot have capital letters in it
 		.setName("show_inactivity")
-		.setDescription("pongs")
-		.addUserOption((option) =>
-		option.setName('user')
-		.setDescription('user to be read')
-		.setRequired(true)),
+		.setDescription("shows a list of inactive users"),
+		
+		//This code below gives discord
+		//users the option of entering information
+		//becasue the setRequired is true the discord user
+		//must add an input.
+		// .addUserOption((option) =>
+		// option.setName('user')
+		// .setDescription('user to be read')
+		// .setRequired(true)),
 
     async execute(interaction) {
 		
 		//This gets a single user within the server.
-		const user = interaction.options.getUser("user");
+		const user_list = interaction.guilds.get(/*process.env.GUILD_ID*/ "1146906893911064626");
+		
 		const addEmbed = new EmbedBuilder()
-			.setTitle('The user is ' + user.tag)
+			.setTitle('The users in this server are ' + user_list)
 			.setColor(0x2ECC71)
 			await interaction.reply({
 				embeds: [addEmbed],
