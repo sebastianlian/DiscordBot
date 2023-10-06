efea1f80133f99ccfb9e266ef8376a02fed0b4a7
Alexis Pardo <alexispardo27@gmail.com>
2023-10-06 17:49:55 -0400

    Updates showInactivity.js

--- src/commands/showInactivity.js
+++ src/commands/showInactivity.js
@@ -3,16 +3,20 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("showinactivity")
-        .setDescription("Shows members who are considered inactive that are eligible to be purged.")
-        .addStringOption((option) =>
-            option
-                .setName("message")
-                .setDescription("the message to echo")
-                .setRequired(true)
-            ),
-    async executr(interaction) {
+        .setDescription("Shows members who are considered inactive that are eligible to be purged."),
+        // .addStringOption((option) =>
+        //     option
+        //         .setName("message")
+        //         .setDescription("the message to echo")
+        //         .setRequired(true)
+        //     ),
+
+    async execute(interaction) {
+        const serverMembers = await interaction.guild.members.fetch();
+        const allMembers = serverMembers.map(member => member.user.tag).join('\n');
+        
         interaction.reply({
-            content: interaction.option.getString("message"),
+            content: allMembers,
             emphemeral: true
         });
     }
