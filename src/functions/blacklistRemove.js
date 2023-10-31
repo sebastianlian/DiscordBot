25433599693cff960ffa74f8645afc42363fb199
Gabriel Kerven <grkerven@gmail.com>
2023-10-31 17:04:26 -0400

    fixed blacklist commands to @ user

--- src/functions/blacklistRemove.js
+++ src/functions/blacklistRemove.js
@@ -1,22 +1,23 @@
 const blacklistSchema = require("../models/blacklistSchema");
-const { channelMention, roleMention, userMention, PermissionFlagsBits } = require('discord.js');
+const { channelMention, roleMention, userMention } = require('discord.js');
 
 
 async function removeBlacklistDB(userid, userTag){
     const userIdString = userid.toString();
+    const mentionedUser = userMention(userid);
     const doc = await blacklistSchema.blackListDB.findOne();
     const exist = await blacklistSchema.blackListDB.findOne({blackListedUsers: userIdString});
     const mention = "insert method";
     if(doc){
         if(exist){
             await blacklistSchema.blackListDB.updateOne({$pull: {blackListedUsers: userIdString}});
-            console.log(`User ${userIdString} has been removed from the Blacklist.`);
-            const resultMessage = (`User ${userTag} has been removed from the Blacklist.`);
+            console.log(`User ${userTag} has been removed from the Blacklist.`);
+            const resultMessage = (`User ${mentionedUser.toString()} has been removed from the Blacklist.`);
             return resultMessage;
         }
         else{
             console.log(`User ${userIdString} is not in the Blacklist`);
-            const resultMessage = (`User ${userTag} is not in the Blacklist`);
+            const resultMessage = (`User ${mentionedUser.toString()} is not in the Blacklist`);
             return resultMessage;
         }
     }
