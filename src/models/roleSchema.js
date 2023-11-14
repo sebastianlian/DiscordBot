6097ab792b534f05c335062dba5aae33ec3e6de3
Gabriel Kerven <grkerven@gmail.com>
2023-11-14 17:13:24 -0500

    fixed schemas

--- src/models/roleSchema.js
+++ src/models/roleSchema.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema({
-    roleId: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      lastMessageDate: {
-        type: Date,
-      },
+  roleId: {
+      type: String,
+      required: true,
+      unique: true,
+  },
+  lastMessageDate: {
+      type: Date,
+  },
 });
 
 const roleDB = mongoose.model("RoleTimeDB", roleSchema);
