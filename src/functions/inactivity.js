6f2d509c36f2fac2ae2fdd75b361366a302f1d50
rsingh021 <ravjotsingh1434@gmail.com>
2023-11-28 02:28:02 -0500

    added users to the inactivity database

--- src/functions/inactivity.js
+++ src/functions/inactivity.js
@@ -25,7 +25,76 @@ const activeMembers = [];
         })
 // }
 
-    module.exports = {
-        // listInactivity,
-        activeMembers,
+//     module.exports = {
+//         // listInactivity,
+//         activeMembers,
+//     }
+
+const inactivitySchema = require("../models/inactivitySchema");
+const activeUsers = [];
+
+function checkInactiveUsers(client) {
+  client.on("messageCreate", (message) => {
+    if (!message.author.bot) {
+        // Check if user is in the list
+      const activeUser = activeUsers.find((user) => user.id === message.author.id); 
+      if (activeUser) {
+        // if they are in the list update the message date
+        activeUser.messageDate = Date.now();
+      } else {
+        //if they are not in the list add the users id and the message date
+        activeUsers.push({ id: message.author.id, messageDate: Date.now() });
+      }
     }
+  });
+}
+
+function getInactiveUsers() {
+  const currentTime = Date.now();
+  // 1 minute timer
+  const inactivityTimer = 1 * 60 * 1000;
+
+  // filters users who have exceded the inactivity timer
+  // inactivitySchema.inactiveDB.create({ userid, lastMessageDate: messageDate });
+  return activeUsers.filter((user) => currentTime - user.messageDate > inactivityTimer);
+
+
+}
+ function addInactivityDB(userid, messageDate){
+  const doc =  inactivitySchema.inactiveDB.findOne();
+  if(doc){
+      try {
+          // Check if the user already exists in the database
+          let user =  inactivitySchema.inactiveDB.findOne({ userId });
+  
+          if (!user) {
+              // If the user doesn't exist, create a new user document
+              user = new Inactivity({
+                  userId,
+                  lastMessageDate: messageDate,
+              });
+          } else {
+              // If the user already exists, update the lastMessageDate
+              user.lastMessageDate = messageDate;
+          }
+  
+          // Save the user document to the database
+           user.save();
+  
+          console.log(`Inactivity data added for user ${userId}`);
+      } catch (error) {
+          console.error('Error adding inactivity data:', error);
+      }
+  }
+  else{
+      inactivitySchema.inactiveDB.create({ userId, lastMessageDate: messageDate });
+      console.log(`New Schema has been created with ${userId} as the first value.`);
+  }
+}
+
+module.exports = {
+     checkInactiveUsers, 
+     getInactiveUsers, 
+     addInactivityDB,
+     activeUsers
+};
