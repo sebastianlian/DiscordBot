39cb19de8f34418160ff7a1af3920ba1a97f3434
rsingh021 <ravjotsingh1434@gmail.com>
2023-12-11 21:22:06 -0500

    Fixed inactivity database
    to properly add users and to update their time when last messaged

--- src/functions/inactivity.js
+++ src/functions/inactivity.js
@@ -1,4 +1,5 @@
 const { blackListDB } = require("../models/blacklistSchema");
+const { inactiveDB } = require("../models/inactivitySchema");
 const activeUsers = [];
 
 async function checkInactiveUsers(client) {
@@ -16,9 +17,20 @@ async function checkInactiveUsers(client) {
         if (activeUser) {
           // if they are in the list update the message date
           activeUser.messageDate = Date.now();
+          // update the database with the new message date
+          await inactiveDB.findOneAndUpdate(
+            { userId: message.author.id },
+            { lastMessageDate: new Date(activeUser.messageDate) },
+            { upsert: true }
+          );
         } else {
-          //if they are not in the list add the users id and the message date
+          // if they are not in the list add the users id and the message date
           activeUsers.push({ id: message.author.id, messageDate: Date.now() });
+          // add the user to the database
+          await inactiveDB.create({
+            userId: message.author.id,
+            lastMessageDate: new Date(Date.now()),
+          });
         }
       }
     }
@@ -27,24 +39,24 @@ async function checkInactiveUsers(client) {
 
 function getInactiveUsers() {
   const currentTime = Date.now();
-  // 10 second timer
-  const inactivityTimer = 10 * 1000;
-  // 1 minute timer
-  //const inactivityTimer = 1 * 60 * 1000;
+// 10 second timer
+const inactivityTimer = 10 * 1000;
+// 1 minute timer
+//const inactivityTimer = 1 * 60 * 1000;
 
-  // 1 hour timer
-  //const inactivityTimer = 1 * 60 * 60 * 1000;
+// 1 hour timer
+//const inactivityTimer = 1 * 60 * 60 * 1000;
 
-  // 1 day timer
-  //const inactivityTimer = 24 * 60 * 60 * 1000;
+// 1 day timer
+//const inactivityTimer = 24 * 60 * 60 * 1000;
 
-  // 1 week timer
-  //const inactivityTimer = 7 * 24 * 60 * 60 * 1000;
+// 1 week timer
+//const inactivityTimer = 7 * 24 * 60 * 60 * 1000;
 
-  // 1 month timer
-  //const inactivityTimer = 4 * 7 * 24 * 60 * 60 * 1000;
+// 1 month timer
+//const inactivityTimer = 4 * 7 * 24 * 60 * 60 * 1000;
 
-  // filters users who have exceded the inactivity timer
+// filters users who have exceded the inactivity timer
   return activeUsers.filter(
     (user) => currentTime - user.messageDate > inactivityTimer
   );
