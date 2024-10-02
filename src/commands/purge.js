f4eab9ef9d8cadfa5cd507a6ea59bcd076a92416
Najee Douglas <ndouglas120@gmail.com>
2024-10-01 21:13:43 -0400

    Modified purge command + schema to track list of purged users

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,102 +1,100 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require("@discordjs/builders");
-const { ButtonStyle, Embed, PermissionFlagsBits, MessageEmbed } = require("discord.js");
-const { checkInactiveUsers, getInactiveUsers, activeUsers } = require("../functions/inactivity");
-const PurgeHistory = require("../models/purgeHistorySchema");
-
-// Log the PurgeHistory model to the console for debugging
-console.log('PurgeHistory model:', PurgeHistory);
+const { ButtonStyle, PermissionFlagsBits } = require("discord.js");
+const { getInactiveUsers } = require("../functions/inactivity");
+const { PurgeHistory } = require("../models/purgeHistorySchema");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("purge")
-		.setDescription("Kicks users who are inactive.")
-		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-	
+    data: new SlashCommandBuilder()
+        .setName("purge")
+        .setDescription("Kicks users who are inactive.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    
     async execute(interaction) {
-		const embed = new EmbedBuilder()
-        	.setTitle("Purge Confrimation")
-        	.setDescription("Would you like to continue with the purge?")
-        	.setColor(0x0099FF)
+        const embed = new EmbedBuilder()
+            .setTitle("Purge Confirmation")
+            .setDescription("Would you like to continue with the purge?")
+            .setColor(0x0099FF)
 
-		interaction.reply({
+        interaction.reply({
             embeds: [embed],
             ephemeral: true,
-			components: [
-				new ActionRowBuilder().setComponents(
-					new ButtonBuilder()
-						.setCustomId("confirm")
-						.setLabel("Confirm")
-						.setStyle(ButtonStyle.Success),
-					new ButtonBuilder()
-						.setCustomId("cancel")
-						.setLabel("Cancel")
-						.setStyle(ButtonStyle.Danger),
-				)
-			]
+            components: [
+                new ActionRowBuilder().setComponents(
+                    new ButtonBuilder()
+                        .setCustomId("confirm")
+                        .setLabel("Confirm")
+                        .setStyle(ButtonStyle.Success),
+                    new ButtonBuilder()
+                        .setCustomId("cancel")
+                        .setLabel("Cancel")
+                        .setStyle(ButtonStyle.Danger),
+                )
+            ]
         });
 
-		const filter = (i) => i.customId === "confirm" || i.customId === "cancel";
-		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+        const filter = (i) => i.customId === "confirm" || i.customId === "cancel";
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
 
-		collector.on("collect", async (buttonInteraction) => {
-			if (buttonInteraction.customId === "confirm") {
-				const inactiveUsers = getInactiveUsers();
-				let purgedCount = 0;
+        collector.on("collect", async (buttonInteraction) => {
+            if (buttonInteraction.customId === "confirm") {
+                const inactiveUsers = getInactiveUsers();
+                const purgedUsers = [];
 
-				// goes through the list of users and gets id
-				for (const user of inactiveUsers) {
-					const member = interaction.guild.members.cache.get(user.id);
+                for (const [userId, userData] of inactiveUsers) {
+                    const member = interaction.guild.members.cache.get(userId);
 
-					if (member) {
-						try {
-							// kicks members who are in the list and updates the purged count
-							await member.kick("Inactive user purge");
-							purgedCount++;
-						} catch (error) {
-							console.error(`Error kicking user ${user.id}: ${error}`);
-						}
-					}
-				}
+                    if (member) {
+                        try {
+                            await member.kick("Inactive user purge");
+                            purgedUsers.push({
+                                userId: userId,
+                                username: member.user.username
+                            });
+                        } catch (error) {
+                            console.error(`Error kicking user ${userId}: ${error}`);
+                        }
+                    }
+                }
 
-				//Log record of purge to database
-				try{
-					await PurgeHistory.create({
-						userId: interaction.user.id,
-						username: interaction.user.username,
-						executionDate: new Date(),
-						purgedCount: purgedCount,
-					});
-				} catch (error) {
-					console.error(`Error logging purge to database: ${error}`);
-				}
+                try {
+                    await PurgeHistory.create({
+                        userId: interaction.user.id,
+                        username: interaction.user.username,
+                        executionDate: new Date(),
+                        purgedCount: purgedUsers.length,
+                        purgedUsers: purgedUsers,
+                    });
+                } catch (error) {
+                    console.error(`Error logging purge to database: ${error}`);
+                }
 
-				const confirmEmbed = new EmbedBuilder()
-					.setTitle("Purge Confirmed")
-					.setDescription("Removed $[purgedCount] inactive users.")
-					.setColor(0x2ECC71);
+                const purgedUserNames = purgedUsers.map(user => user.username).join(", ");
+                const confirmEmbed = new EmbedBuilder()
+                    .setTitle("Purge Confirmed")
+                    .setDescription(`Removed ${purgedUsers.length} inactive users: ${purgedUserNames}`)
+                    .setColor(0x2ECC71);
 
-				await buttonInteraction.update({
-					embeds: [confirmEmbed],
-					components: [],
-				});
-			} else {
-				const cancelEmbed = new EmbedBuilder()
-					.setTitle("Purge Canceled")
-					.setDescription("No users removed.")
-					.setColor(0xE74C3C);
+                await buttonInteraction.update({
+                    embeds: [confirmEmbed],
+                    components: [],
+                });
+            } else {
+                const cancelEmbed = new EmbedBuilder()
+                    .setTitle("Purge Canceled")
+                    .setDescription("No users removed.")
+                    .setColor(0xE74C3C);
 
-				await buttonInteraction.update({
-					embeds: [cancelEmbed],
-					components: [],
-				});
-			}
-		});
+                await buttonInteraction.update({
+                    embeds: [cancelEmbed],
+                    components: [],
+                });
+            }
+        });
 
-		collector.on("end", (collected, reason) => {
-			if (reason === "time") {
-				interaction.followUp("Purge confirmation timed out.");
-			}
-		});
-		
-	}
+        collector.on("end", (collected, reason) => {
+            if (reason === "time") {
+                interaction.followUp("Purge confirmation timed out.");
+            }
+        });
+    }
 };
