4fbfbe04db3f0cad40abc50ac0a39b46127f0b19
Zeshriel <elaccruche@ggc.edu>
2024-10-18 17:14:01 -0400

    update the main with timer

--- src/commands/timer.js
+++ src/commands/timer.js
@@ -1,7 +1,10 @@
 //have timer.js show the amount of time left until the purge will execute
+
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
-const { time, PermissionFlagsBits, MessageEmbed, Embed } = require("discord.js");
+const { time, PermissionFlagsBits } = require("discord.js");
 const setPurgeDate = require("./setpurge");
+const { getInactiveUsers, addOrUpdateInactivityDB } = require("../functions/inactivity"); // Importing functions to handle inactivity
+const PURGE_TIMER = 10 * 1000; // Example: 10 seconds for testing
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,21 +13,38 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
+    // Get the purge days set by setpurge
     const purgeDays = setPurgeDate.getPurgeDays();
-    const days = purgeDays;
     const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + days);
-    const timeString = time(currentDate);
-    const dateString = currentDate.toLocaleDateString();
+    currentDate.setDate(currentDate.getDate() + purgeDays);
     const relative = time(currentDate, "R");
 
-    const timerEmbed = new EmbedBuilder()
-        .setTitle("Timer")
-        .setDescription(`The date and time ${days} days from today will be: ${dateString} ${timeString}`);
+    // Fetch inactive users who exceed the PURGE_TIMER
+    const inactiveUsers = await getInactiveUsers();
+    const currentTime = Date.now();
+    const usersToPurge = [];
 
+    // Loop over inactive users to check if they exceed the PURGE_TIMER
+    for (const [userId, userInfo] of inactiveUsers.entries()) {
+      const inactivityDuration = currentTime - userInfo.lastMessageDate;
+
+      if (inactivityDuration > PURGE_TIMER) {
+        // Add user to the purge list
+        await addOrUpdateInactivityDB(userId, userInfo.userName, userInfo.lastMessageDate);
+        usersToPurge.push(userInfo.userName);
+      }
+    }
+
+    // Create a response with the inactive users who will be purged
+    const timerEmbed = new EmbedBuilder()
+      .setTitle("Purge Timer")
+      .setDescription(`The purge is scheduled for ${relative}.\n\nUsers marked for purging: \n${usersToPurge.length ? usersToPurge.join(", ") : "No users to purge."}`)
+      .setColor(0x0099ff);
+
+    // Reply with the embedded message
     await interaction.reply({
       embeds: [timerEmbed],
-      ephemeral: true
+      ephemeral: true,
     });
   },
 };
